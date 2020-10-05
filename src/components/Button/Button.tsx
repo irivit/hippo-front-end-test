b6389993d5b9a48dbf832772e2684a48f7d9aeef
irivi <copacies@gmail.com>
2020-10-05 13:11:26 -0500

    Link added to the header products button

--- src/components/Button/Button.tsx
+++ src/components/Button/Button.tsx
@@ -8,7 +8,7 @@ import './Button.scss';
 export default function ContainedButtons() {
   return (
     <div className="header-div-products">
-    <Button variant="contained" color="primary" className="header-div-products-button" size="large">
+    <Button variant="contained" color="primary" className="header-div-products-button" size="large" href="https://www.hippoed.com/">
       Explore our products
     </Button>
 
