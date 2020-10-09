fed210609eaf64ec99171b709b09e1c19ed13511
irivi <copacies@gmail.com>
2020-10-08 21:49:19 -0500

    code cleaned

--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -2,11 +2,9 @@ import React from 'react';
 
 // Material UI
 import AppBar from '@material-ui/core/AppBar';
-import Button from '@material-ui/core/Button';
 import IconButton from '@material-ui/core/IconButton';
 import MenuIcon from '@material-ui/icons/Menu';
-import Grid from '@material-ui/core/Grid';
-import Divider from '@material-ui/core/Divider';
+
 
 //Style
 import './Navbar.scss';
